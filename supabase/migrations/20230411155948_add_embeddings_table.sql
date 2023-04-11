f1fdaf0ab40c8ed2afc3628dbcfd4503df83906f
will249 <willa@theodo.co.uk>
2023-04-11 17:09:17 +0100

    feat: add-initial-db-migrations

---
+++ supabase/migrations/20230411155948_add_embeddings_table.sql
@@ -0,0 +1,7 @@
+create table embedding (
+  id bigint generated always as identity primary key,
+  vector vector(1536) not null,
+  input_text text not null,
+  input_url text not null,
+  usage_count bigint default 0
+);
