10d3f6fc2fa7335c2ba884819cf8cc8a9d0ea82e
Claire Squires <claires@theodo.co.uk>
2023-04-14 11:34:43 +0100

    feat: extending scraping for integration into main.py

--- src/app/utils/python/scraping.py
+++ src/app/utils/python/scraping.py
@@ -5,24 +5,30 @@ from scrapy.crawler import CrawlerProcess
 
 class Spider(scrapy.Spider):
     name = 'spider'
-    start_urls = ['https://wizzair.com/en-gb/information-and-services/booking-information/how-to-book', 'https://wizzair.com/en-gb/information-and-services/booking-information/check-in-and-boarding', 'https://wizzair.com/en-gb/information-and-services/booking-information/changing-your-reservation']
+    start_urls = ['https://wizzair.com/en-gb/information-and-services/booking-information/how-to-book', 'https://wizzair.com/en-gb/information-and-services/booking-information/check-in-and-boarding',
+                  'https://wizzair.com/en-gb/information-and-services/booking-information/changing-your-reservation']
 
     def parse(self, response):
-        title = response.css('title::text').get() # might be useful in the future?
-
+        # might be useful in the future?
+        title = response.css('title::text').get()
         article_info = response.css("article#info")
         article_info_text = article_info.css("::text").getall()
         article_info_text_clean = clean_scraped_text(article_info_text)
 
-        print("NEW PAGE")
+        # print("NEW PAGE")
         print(response.url)
-        print(repr(article_info_text_clean))
-        print('\n\n\n')
+        # print(repr(article_info_text_clean))
+        # print('\n\n\n')
 
-        return {
+        yield {
             'url': response.url,
             'article_info_text': article_info_text_clean
         }
+
+    def handle_scraped_pages(self, result):
+        print('handling scraped pages')
+        print(result)
+
 
 def clean_scraped_text(text):
     text_stripped = [t.strip() for t in text if t.strip()]
@@ -31,14 +37,14 @@ def clean_scraped_text(text):
     text_quotes = re.sub('"', "'", text_regexed)
     return text_quotes
 
-def handle_scraped_pages(result):
-    print('handling scraped pages')
-    print(result)
 
 if __name__ == "__main__":
     process = CrawlerProcess(settings={
         'LOG_LEVEL': 'CRITICAL'
     })
-    deffered = process.crawl(Spider)
-    deffered.addCallback(handle_scraped_pages)
-    process.start() # the script will block here until the crawling is finished
+    result = process.crawl(Spider)
+    # print(result)
+    process.crawl(Spider)
+    # deffered.addCallback(handle_scraped_pages)
+    process.start()  # the script will block here until the crawling is finished
+    print(result)
