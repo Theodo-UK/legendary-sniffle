933034024ee6867abec11625dcc4967e858db47f
matt-newhall <matthewn@theodo.co.uk>
2023-04-17 12:55:50 +0100

    fix: responded to comments

--- src/app/pages/api/openai.ts
+++ src/app/pages/api/openai.ts
@@ -1,11 +1,17 @@
+import * as path from 'path';
+import dotenv from 'dotenv';
 import { OpenaiApiType } from '@/app/types/OpenaiApiType';
 
+dotenv.config({ path: path.resolve(__dirname, '../..', '.env.local') });
+
 const fetchData = async (): Promise<OpenaiApiType> => {
+  // source: https://supabase.com/docs/guides/getting-started/openai/vector-search
   const temperamentPrimer =
     'You are a helpdesk assistant for WizzAir who loves to help users find the correct information for their questions from our FAQs.';
   const knowledgeBasePrimer =
     'If you are unsure and the answer is not explicitly written in the information provided, say "Sorry, I don\'t know how to help with that.';
   const legalityPrimer = 'You should not provide legal advice of any kind.';
+  const userDummyMessage = 'This is a test message!';
 
   const response = await fetch('https://api.openai.com/v1/chat/completions', {
     method: 'POST',
@@ -19,6 +25,7 @@ const fetchData = async (): Promise<OpenaiApiType> => {
         { role: 'system', content: temperamentPrimer },
         { role: 'system', content: knowledgeBasePrimer },
         { role: 'system', content: legalityPrimer },
+        { role: 'user', content: userDummyMessage },
       ],
       temperature: 0.1,
     }),
@@ -28,6 +35,7 @@ const fetchData = async (): Promise<OpenaiApiType> => {
   return data as OpenaiApiType;
 };
 
-fetchData().then((response) =>
-  console.log(response.choices[0].message.content)
+fetchData().then(
+  (response) => console.log(response)
+  // console.log(response.choices[0].message.content)
 );
