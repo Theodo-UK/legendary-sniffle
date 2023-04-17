83241370a0a3c0943a3223f4af1d91f7b6d2272a
matt-newhall <matthewn@theodo.co.uk>
2023-04-17 11:11:22 +0100

    feat: added primer for chatgpt

--- src/app/pages/api/openai.ts
+++ src/app/pages/api/openai.ts
@@ -1,6 +1,12 @@
 import { OpenaiApiType } from '@/app/types/OpenaiApiType';
 
 const fetchData = async (): Promise<OpenaiApiType> => {
+  const temperamentPrimer =
+    'You are a helpdesk assistant for WizzAir who loves to help users find the correct information for their questions from our FAQs.';
+  const knowledgeBasePrimer =
+    'If you are unsure and the answer is not explicitly written in the information provided, say "Sorry, I don\'t know how to help with that.';
+  const legalityPrimer = 'You should not provide legal advice of any kind.';
+
   const response = await fetch('https://api.openai.com/v1/chat/completions', {
     method: 'POST',
     headers: {
@@ -9,8 +15,12 @@ const fetchData = async (): Promise<OpenaiApiType> => {
     },
     body: JSON.stringify({
       model: 'gpt-3.5-turbo',
-      messages: [{ role: 'user', content: 'Say this is a test!' }],
-      temperature: 0.7,
+      messages: [
+        { role: 'system', content: temperamentPrimer },
+        { role: 'system', content: knowledgeBasePrimer },
+        { role: 'system', content: legalityPrimer },
+      ],
+      temperature: 0.1,
     }),
   });
 
