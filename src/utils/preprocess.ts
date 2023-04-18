259990da9cee9c38cf27da8fc55b0c470b274b72
Claire Squires <claires@theodo.co.uk>
2023-04-18 16:40:11 +0100

    feat: created bash script preprocess.sh (and tested)

--- src/utils/preprocess.ts
+++ src/utils/preprocess.ts
@@ -8,18 +8,15 @@ import { createClient } from '@supabase/supabase-js';
 dotenv.config({ path: path.resolve(__dirname, '../..', '.env.local') });
 
 const preprocess = async () => {
-  const chunks = JSON.parse(fs.readFileSync('../../data/chunks.json', 'utf8'));
+  const chunks = JSON.parse(fs.readFileSync('data/chunks.json', 'utf8'));
   const configuration = new Configuration({
     apiKey: process.env.OPENAI_API_KEY,
   });
   const openai = new OpenAIApi(configuration);
-  // console.log(chunks);
   const outputArray = await createEmbeddings(openai, chunks);
-  console.log('check');
-  console.log(outputArray);
 
   fs.writeFile(
-    '../../data/embeddings.json',
+    'data/embeddings.json',
     JSON.stringify(outputArray),
     'utf8',
     (error) => {
