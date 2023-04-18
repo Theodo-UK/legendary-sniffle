7bb96c871f3b183a693c69f908d4873fdb2cc5a6
matt-newhall <matthewn@theodo.co.uk>
2023-04-18 11:19:05 +0100

    feat: added custom knowledge to chatgpt call

--- src/app/pages/api/openai.ts
+++ src/app/pages/api/openai.ts
@@ -4,14 +4,27 @@ import { OpenaiApiType } from '@/app/types/OpenaiApiType';
 
 dotenv.config({ path: path.resolve(__dirname, '../..', '.env.local') });
 
-const fetchData = async (): Promise<OpenaiApiType> => {
+type getChatResponseType = {
+  knowledge: string[];
+};
+
+const getChatResponse = async (
+  props: getChatResponseType
+): Promise<OpenaiApiType> => {
   // source: https://supabase.com/docs/guides/getting-started/openai/vector-search
   const temperamentPrimer =
     'You are a helpdesk assistant for WizzAir who loves to help users find the correct information for their questions from our FAQs.';
   const knowledgeBasePrimer =
     'If you are unsure and the answer is not explicitly written in the information provided, say "Sorry, I don\'t know how to help with that.';
   const legalityPrimer = 'You should not provide legal advice of any kind.';
+
   const userDummyMessage = 'This is a test message!';
+
+  const customKnowledge = props.knowledge
+    ? props.knowledge.map((info) => {
+        return { role: 'system', content: info };
+      })
+    : [];
 
   const response = await fetch('https://api.openai.com/v1/chat/completions', {
     method: 'POST',
@@ -25,6 +38,7 @@ const fetchData = async (): Promise<OpenaiApiType> => {
         { role: 'system', content: temperamentPrimer },
         { role: 'system', content: knowledgeBasePrimer },
         { role: 'system', content: legalityPrimer },
+        ...customKnowledge,
         { role: 'user', content: userDummyMessage },
       ],
       temperature: 0.1,
@@ -35,7 +49,7 @@ const fetchData = async (): Promise<OpenaiApiType> => {
   return data as OpenaiApiType;
 };
 
-fetchData().then(
+getChatResponse({ knowledge: [] }).then(
   (response) => console.log(response)
   // console.log(response.choices[0].message.content)
 );
