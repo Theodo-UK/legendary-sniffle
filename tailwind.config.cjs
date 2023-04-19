7837ea8f47ea33686020bb4dca58da2230362a1f
will249 <willa@theodo.co.uk>
2023-04-19 17:55:03 +0200

    feat: add search bar styling

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,38 @@
 module.exports = {
   content: ["./src/**/*.tsx"],
   theme: {
-    extend: {},
+    extend: {      
+      fontFamily: {
+      montserrat: ['Montserrat', 'sans-serif'],
+      cobeHeavy: ['CobeHeavy', 'sans-serif'],
+      cobeSemiBold: ['CobeSembiBold', 'sans-serif'],
+      lato: ['Lato', 'sans-serif'],
+    },
+    colors: {
+      offwhite: '#cbcbcb',
+      white: '#fff',
+      black: '#000',
+      green: '#6abd9e',
+      dark_green: '#33a852',
+      yellow: '#fabc05',
+      orange: '#fe6d01',
+      blue: '#17539F',
+      light_blue: '#4284f3',
+      red: '#ea4335',
+      light_grey: '#e1e1e1',
+      x_light_grey: '#dadada',
+      grey: '#666666',
+    },
+    spacing: {
+      xxs: '2px',
+      xs: '4px',
+      sm: '8px',
+      md: '16px',
+      lg: '24px',
+      xl: '40px',
+      '2xl': '400px',
+    },
+    },
   },
   plugins: [],
 };
