61a18857279ea1cae42c9b6ea9581f4ee49faec6
Simon Lake <simonl@theodo.co.uk>
2023-04-21 09:57:07 +0100

    refactor: remove unused imports

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,8 +1,5 @@
 import { callOpenai } from '@/utils/openai';
 import { NextResponse } from 'next/server';
-import dotenv from 'dotenv';
-import path from 'path';
-dotenv.config({ path: path.resolve(__dirname, '../..', '.env.local') });
 
 export async function POST(request: Request) {
   const requestBody = await request.json();
