b558273312182cd9932ba57056800c72a8cc6e49
Simon Lake <simonl@theodo.co.uk>
2023-04-24 14:38:04 +0100

    feat: add icon

--- src/components/atoms/ExampleQuestion/ExampleQuestion.tsx
+++ src/components/atoms/ExampleQuestion/ExampleQuestion.tsx
@@ -1,5 +1,8 @@
 'use client';
 
+import Image from 'next/image';
+import icon from '@/public/images/communicate.png';
+
 interface ExampleQuestionProps {
   question: string;
   onClick: (question: string) => void;
@@ -11,9 +14,10 @@ export const ExampleQuestion = ({
 }: ExampleQuestionProps) => {
   return (
     <button
-      className="rounded-md sm:text-sm w-full text-left hover:bg-light_grey p-5"
+      className="rounded-md sm:text-sm w-full text-left hover:bg-light_grey p-3 flex items-center gap-3"
       onClick={() => onClick(question)}
     >
+      <Image alt="" src={icon} width={32} height={32} />
       {question}
     </button>
   );
