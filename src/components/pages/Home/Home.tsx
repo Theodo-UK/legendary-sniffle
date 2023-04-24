3c16f37d019d9f52c9eb0066ccc50cafadb1b77f
matt-newhall <matthewn@theodo.co.uk>
2023-04-24 15:09:22 +0100

    feat: added header component

--- src/components/pages/Home/Home.tsx
+++ src/components/pages/Home/Home.tsx
@@ -1,6 +1,7 @@
 import { ChatbotResponse } from '@/components/atoms/ChatbotResponse/ChatbotResponse';
 import { TextInput } from '@/components/atoms/TextInput/TextInput';
 import { OutputWindow } from '@/components/layout/OutputWindow/OutputWindow';
+import { Header } from '@/components/molecules/Header/Header';
 import useHome from '@/utils/useHome';
 
 const Home = () => {
@@ -8,7 +9,7 @@ const Home = () => {
 
   return (
     <OutputWindow>
-      <div className="text-center h-24 bg-white">Logo</div>
+      <Header />
       <div>Question</div>
       <div className="h-full">
         {chatbotResponse && <ChatbotResponse {...chatbotResponse} />}
