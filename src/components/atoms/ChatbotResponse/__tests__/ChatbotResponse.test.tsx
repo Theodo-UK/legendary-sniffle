17de2bc291bf74be3d04f36f9e30a484742882ad
matt-newhall <matthewn@theodo.co.uk>
2023-04-24 12:50:54 +0100

    fix: fixed failing test

--- src/components/atoms/ChatbotResponse/__tests__/ChatbotResponse.test.tsx
+++ src/components/atoms/ChatbotResponse/__tests__/ChatbotResponse.test.tsx
@@ -2,9 +2,16 @@ import { axe } from 'jest-axe';
 import { act, render } from '@testing-library/react';
 import { ChatbotResponse } from '../ChatbotResponse';
 
-const mockMessage = 'blank';
+const mockResponse = { chatMessage: 'blank', associatedUrl: '' };
+const mockResponseUrl = {
+  chatMessage: 'blank',
+  associatedUrl: 'https://localhost:somewhere',
+};
 
-it.each([['Output', <ChatbotResponse message={mockMessage} />]])(
+it.each([
+  ['Output', <ChatbotResponse {...mockResponse} />],
+  ['Output withUrl', <ChatbotResponse {...mockResponseUrl} />],
+])(
   '%s should not have any basic accessibility issues',
   async (name, Component) => {
     const { container } = render(Component);
