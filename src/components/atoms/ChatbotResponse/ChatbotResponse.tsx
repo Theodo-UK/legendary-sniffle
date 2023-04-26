e808105cc5847a2add2e7ee8500048139f4e4922
matt-newhall <matthewn@theodo.co.uk>
2023-04-26 15:44:43 +0100

    fix: bug with lack of newlines fixed

--- src/components/atoms/ChatbotResponse/ChatbotResponse.tsx
+++ src/components/atoms/ChatbotResponse/ChatbotResponse.tsx
@@ -4,6 +4,7 @@ import Image from 'next/image';
 import icon from '@public/response.png';
 
 export const ChatbotResponse = (props: CallOpenaiType) => {
+  console.log(props.chatMessage);
   return (
     <div className="rounded-md sm:text-sm w-full text-left p-3 flex gap-3 items-start">
       <>
@@ -11,20 +12,24 @@ export const ChatbotResponse = (props: CallOpenaiType) => {
           <Image alt="" src={icon} width={32} height={32} />
         </div>
 
-        <p>
-          {`${props.chatMessage}`}
-          {props.associatedUrl.length > 0 && (
-            <>
-              <br />
-              <br />
-              You can find out more information about this{' '}
-              <a href={props.associatedUrl} target="_blank">
-                <span className="font-bold text-wizz_pink underline">here</span>
-              </a>
-              .
-            </>
-          )}
-        </p>
+        <div className="whitespace-pre-line">
+          <p>
+            {`${props.chatMessage}`}
+            {props.associatedUrl.length > 0 && (
+              <>
+                <br />
+                <br />
+                You can find out more information about this{' '}
+                <a href={props.associatedUrl} target="_blank">
+                  <span className="font-bold text-wizz_pink underline">
+                    here
+                  </span>
+                </a>
+                .
+              </>
+            )}
+          </p>
+        </div>
       </>
     </div>
   );
